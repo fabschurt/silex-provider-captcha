8bdffef4e5ab5206e980a57ab15c6003c40a89f6
Fabien Schurter <fabien@fabschurt.com>
2016-11-02 21:55:25 +0100

    Correct copyright year in form type class

--- src/Form/Type/CaptchaType.php
+++ src/Form/Type/CaptchaType.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the fabschurt/silex-provider-captcha package.
  *
- * (c) 2015-2016 Fabien Schurter <fabien@fabschurt.com>
+ * (c) 2016 Fabien Schurter <fabien@fabschurt.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
