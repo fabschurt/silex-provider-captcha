21e1745add20120e8e67df50e165e77b878e53b7
Fabien Schurter <fabien@fabschurt.com>
2016-11-02 18:17:17 +0100

    Refactor test form factory process

---
+++ src/Form/Type/CaptchaType.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the fabschurt/silex-provider-captcha package.
+ *
+ * (c) 2015-2016 Fabien Schurter <fabien@fabschurt.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FabSchurt\Silex\Provider\Captcha\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class CaptchaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        // Make sure the input’s value is always empty (the user must type the captcha every time they send the form)
+        $view->vars['value'] = null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'captcha-input',
+            ],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return TextType::class;
+    }
+}
--- tests/CaptchaServiceProviderTest.php
+++ tests/CaptchaServiceProviderTest.php
@@ -12,9 +12,11 @@
 namespace FabSchurt\Silex\Provider\Captcha\Tests;
 
 use FabSchurt\Silex\Provider\Captcha\CaptchaServiceProvider;
+use FabSchurt\Silex\Provider\Captcha\Form\Type\CaptchaType;
 use Silex\Application;
 use Silex\Provider;
 use Silex\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -33,6 +35,20 @@ final class CaptchaServiceProviderTest extends WebTestCase
         )->same('image/jpeg');
     }
 
+    public function testFormWidgetOutput()
+    {
+        $captchaFormRow = (new Crawler($this->buildFormHtml()))->filter('#form > div')->eq(0);
+        verify(
+            $captchaFormRow
+                ->filter(
+                    '.captcha-wrapper > .captcha-refresh-btn,'.
+                    '.captcha-wrapper > .img.captcha-img,'.
+                    'input[type="text"].captcha-input'
+                )
+                ->count()
+        )->same(3);
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -44,6 +60,14 @@ final class CaptchaServiceProviderTest extends WebTestCase
         ]);
         unset($app['exception_handler']);
         $app->register(new Provider\SessionServiceProvider());
+        $app->register(new Provider\FormServiceProvider());
+        $app->register(new Provider\LocaleServiceProvider());
+        $app->register(new Provider\TranslationServiceProvider());
+        $app->register(new Provider\TwigServiceProvider(), [
+            'twig.templates' => [
+                'form_test' => '{{ form(form) }}',
+            ],
+        ]);
         $app->register(new CaptchaServiceProvider());
 
         return $app;
@@ -56,5 +80,22 @@ final class CaptchaServiceProviderTest extends WebTestCase
     {
         parent::setup();
         $this->client = $this->createClient();
+    }
+
+    /**
+     * Returns an HTML form containing only the captcha row/widget.
+     *
+     * @return string
+     */
+    private function buildFormHtml()
+    {
+        return $this->app['twig']->render('form_test', [
+            'form' => $this->app['form.factory']
+                ->createBuilder()
+                ->add('captcha', CaptchaType::class)
+                ->getForm()
+                ->createView()
+            ,
+        ]);
     }
 }
