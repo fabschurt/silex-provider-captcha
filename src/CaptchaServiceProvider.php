55fd78971c245208b03eb6c289e2bf2c02435a8e
Fabien Schurter <fabien@fabschurt.com>
2016-11-13 01:53:48 +0100

    Initialize stored captcha on application boot

--- src/CaptchaServiceProvider.php
+++ src/CaptchaServiceProvider.php
@@ -95,6 +95,7 @@ final class CaptchaServiceProvider implements ServiceProviderInterface, Bootable
     public function boot(Application $app)
     {
         $app->mount('', $this);
+        $app['captcha']->generate(); // Initialize stored captcha so its value is never empty
     }
 
     /**
